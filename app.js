4e15faa9f83c2374233f0de68f7684826878c970
Benatovich <sambenatovich@gmail.com>
2021-12-07 18:26:43 -0500

    code displays all flavors in the flavor array for my chips array. not sure if that's right

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // import functions
 import { renderCandy } from './render-candy.js';
 import { candies } from './candies-data.js';
+import { renderChips } from './render-chips.js';
+import { chips } from './chips-array.js';
+
 
 // grab DOM elements
 const candyListEl = document.getElementById('candy-list');
+const chipsListEl = document.getElementById('chips-list');
 
 // loop through arrays, render elements, append those elements for each item
 for (let candy of candies) {
@@ -11,6 +15,12 @@ for (let candy of candies) {
     candyListEl.append(candyEl);
 }
 
+for (let chip of chips) {
+    const chipsEl = renderChips(chip);
+    chipsListEl.append(chipsEl);
+}
+
+console.log(chipsListEl);
 console.log(candyListEl);
 
 
