915e9f78cb827ccc7830f8c101a47949269a88ce
Benatovich <sambenatovich@gmail.com>
2021-12-07 21:15:29 -0500

    IP isn't listing the nested objects properly but i'm done for the night

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // import functions
 import { renderCandy } from './render-candy.js';
 import { candies } from './candies-data.js';
@@ -5,12 +6,15 @@ import { renderChips } from './render-chips.js';
 import { chips } from './chips-data.js';
 import { renderColors } from './render-colors.js';
 import { colors } from './colors-data.js';
+import { IPs } from './IP-data.js';
+import { renderIPs } from './render-IPs.js';
 
 
 // grab DOM elements
 const candyListEl = document.getElementById('candy-list');
 const chipsListEl = document.getElementById('chips-list');
 const colorListEl = document.getElementById('color-list');
+const IPListEl = document.getElementById('IP-list');
 
 // loop through arrays, render elements, append those elements for each item
 for (let candy of candies) {
@@ -28,9 +32,15 @@ for (let color of colors) {
     colorListEl.append(colorsEl);
 }
 
+for (let IP of IPs) {
+    const IPsEl = renderIPs(IP);
+    IPListEl.append(IPsEl);
+}
+
 console.log(chipsListEl);
 console.log(candyListEl);
 console.log(colorListEl);
+console.log(IPListEl);
 
 
   // OLD CODE 
--- render-IPs.js
+++ render-IPs.js
@@ -1,3 +1,27 @@
 export function renderIPs(IP) {
-    const div = document.createElement
+    const div = document.createElement('div');
+    const titleEl = document.createElement('p');
+    const genreEl = document.createElement('p');
+    const protagonistDiv = document.createElement('div');
+    const nameEl = document.createElement('p');
+    const factionEl = document.createElement('p');
+    const equipmentEl = document.createElement('p');
+
+    div.classList.add('IP-item');
+    titleEl.classList.add('title');
+    genreEl.classList.add('genre');
+    protagonistDiv.classList.add('protagonist');
+    nameEl.classList.add('name');
+    factionEl.classList.add('faction');
+    equipmentEl.classList.add('equipment');
+
+    titleEl.textContent = IP.title;
+    genreEl.textContent = IP.genre;
+    nameEl.textContent = IP.protagonist.name;
+    factionEl.textContent = IP.protagonist.faction;
+    equipmentEl.textContent = IP.protagonist.equipment;
+
+    div.append(titleEl, genreEl, protagonistDiv);
+
+    return div;
 }
